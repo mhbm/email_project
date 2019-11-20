be58ef35d74bb73e70b8b64cd22b413fd14d6fac
Mateus Macedo <mhbassomacedo@gmail.com>
2019-11-20 12:21:43 -0300

    Create a function that read email from GMAIL using the NODE-IMAP lib
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- src/app/controller/EmailController.ts
+++ src/app/controller/EmailController.ts
@@ -5,8 +5,11 @@ import nodemailer = require("nodemailer");
 import { Email } from "../../config/email/model/email.model";
 import { Message } from "../../config/email/model/message.model";
 
+import Imap = require("imap");
+import { imapConfig } from "./imapConfig";
+
 class EmailController {
-  public create(req: Request, res: Response) {
+  public sendMail(req: Request, res: Response) {
     const body = req.body;
 
     //Passando os parametros para a criação do email
@@ -36,9 +39,69 @@ class EmailController {
     transportEmail.sendMail(message.messageJSON(), (error, info) => {
       if (error) {
         console.log("Erro ao enviar a mensagem", error);
-        res.status(401).json(error);
+        return res.status(401).json(error);
       }
-      res.status(200).json(info);
+      return res.status(200).json(info);
+    });
+  }
+
+  public readMailBox(req: Request, res: Response) {
+    let configuration = new imapConfig();
+    console.log("Connecting to %s", configuration.host);
+    let imapServer = new Imap({
+      user: configuration.user,
+      password: configuration.password,
+      host: configuration.host,
+      port: configuration.port,
+      tls: configuration.tls,
+      tlsOptions: { rejectUnauthorized: false } //,
+      //debug: console.log
+    });
+
+    imapServer.connect();
+
+    imapServer.once("ready", function() {
+      console.log("Server status: %s", imapServer.state);
+      imapServer.openBox("INBOX", true, function(err, box) {
+        if (err) throw err;
+        let f = imapServer.seq.fetch(box.messages.total + ":*", {
+          bodies: ["TEXT", "HEADER.FIELDS (FROM TO SUBJECT DATE)"],
+          struct: true
+        });
+        f.on("message", function(msg, seqno) {
+          console.log("Message #%d", seqno);
+          var prefix = "(#" + seqno + ") ";
+          msg.on("body", function(stream, info) {
+            var buffer = "",
+              count = 0;
+            stream.on("data", function(chunk) {
+              count += chunk.length;
+              buffer += chunk.toString("utf8");
+              console.log("N2º =>", buffer, count);
+            });
+            stream.once("end", function() {
+              /*
+              console.log(
+                prefix + "Parsed header: %s",
+                Imap.parseHeader(buffer)
+              );
+              */
+            });
+          });
+          f.once("error", function(err) {
+            console.log("Fetch error: " + err);
+          });
+          f.once("end", function() {
+            console.log("Done fetching all messages!");
+            imapServer.end();
+          });
+        });
+      });
+      res.status(200).json("OK");
+    });
+    imapServer.once("error", function(err) {
+      console.log("Connection error: aaaaa" + err.stack);
+      return res.status(401).json(err);
     });
   }
 }
---
+++ src/app/controller/imapConfig.ts
@@ -0,0 +1,7 @@
+export class imapConfig {
+  user: string = "testeplusoft14@gmail.com";
+  password: string = "igaindaexiste?";
+  host: string = "imap.gmail.com";
+  port: number = 993;
+  tls: boolean = true;
+}
