6853e135506ec0fced4f8bf3645c70e420122d15
Mateus Macedo <mhbassomacedo@gmail.com>
2019-11-27 00:22:23 -0300

    Refactoring the code. Create another route for start a job by email
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- src/app/controller/imapConfig.ts
+++ src/app/controller/imapConfig.ts
@@ -1,25 +1,33 @@
+import Imap = require("imap");
+
 export class imapConfig {
   host: string;
   port: number;
   tls: boolean;
 
-  constructor(
-    public user: string,
-    public password: string
-  ) {}
+  constructor(public user: string, public password: string) {}
 
   configurationGmail() {
-    this.host = 'imap.gmail.com'
-    this.port = 993
-    this.tls = true
+    this.host = "imap.gmail.com";
+    this.port = 993;
+    this.tls = true;
   }
 
   configurationOffice365() {
-    this.host = 'outlook.office365.com'
-    this.port = 993
-    this.tls = true
+    this.host = "outlook.office365.com";
+    this.port = 993;
+    this.tls = true;
   }
-  
 
-
+  createImap() {
+    return new Imap({
+      user: this.user,
+      password: this.password,
+      host: this.host,
+      port: this.port,
+      tls: this.tls,
+      tlsOptions: { rejectUnauthorized: false } //,
+      //debug: console.log
+    });
+  }
 }
